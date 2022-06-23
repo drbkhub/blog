5e4d1ddf345efdc78851d4b11a6d502a0c6ba157
unknown <drjabaka@gmail.com>
2022-06-24 00:11:50 +0300

    hover link

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class ArticleListView(ListView):
     model = Article
-    paginate_by = 1  # if pagination is desired
+    paginate_by = 10  # if pagination is desired
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
@@ -27,7 +27,7 @@ class ArticleDetailView(DetailView):
 class UserArticlesListView(ListView):
     model = Article
     template_name = 'blog/user_articles_list.html'
-    paginate_by = 1
+    paginate_by = 10
 
     def get_queryset(self):
         queryset = Article.objects.filter(author__id=self.kwargs.get("pk"))
